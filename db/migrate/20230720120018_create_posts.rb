d2d1b28e44d6dca74d083798e341e94e74c666f0
Koshin Hanaki <kousin.0512@gmail.com>
2023-08-03 19:41:25 +0900

    マイグレーションファイルを変更

--- db/migrate/20230720120018_create_posts.rb
+++ db/migrate/20230720120018_create_posts.rb
@@ -1,19 +1,20 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
-  # def change
-    # change_table :posts do |t|
-    #   t.references :user, null: false, foreign_key: true
-    #   t.string :image, null: false
-    #   t.string :caption, null: false
-    #   t.float :latitude
-    #   t.float :longitude
-    #   t.timestamps
-    # end
-    # end
-  create_table :posts do |t|
-    t.references :user, null: false, foreign_key: true
-    t.string :body, null: false
-    t.float :latitude
-    t.float :longitude
-    t.timestamps
+  def change
+    create_table :posts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :body, null: false
+      t.float :latitude
+      t.float :longitude
+      t.timestamps
+    end
   end
 end
+
+# change_table :posts do |t|
+#   t.string :image, null: false
+#   t.references :user, null: false, foreign_key: true
+#   t.string :caption, null: false
+#   t.float :latitude
+#   t.float :longitude
+#   t.timestamps
+# end
