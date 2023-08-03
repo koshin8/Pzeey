ffcd005eb4e167864926f88cd0285570bdbce545
Koshin Hanaki <kousin.0512@gmail.com>
2023-08-03 19:26:45 +0900

    マイグレーションファイル問題を解決

--- db/migrate/20230722104554_remove_image_from_posts.rb
+++ db/migrate/20230722104554_remove_image_from_posts.rb
@@ -1,5 +1,5 @@
-# class RemoveImageFromPosts < ActiveRecord::Migration[7.0]
-#     def change
-#       remove_column :posts, :image, :string
-#     end
-# end
+class RemoveImageFromPosts < ActiveRecord::Migration[7.0]
+    # def change
+    #   remove_column :posts, :image, :string
+    # end
+end
--- db/migrate/20230722115026_rename_caption_to_body_in_posts.rb
+++ db/migrate/20230722115026_rename_caption_to_body_in_posts.rb
@@ -1,5 +1,5 @@
 class RenameCaptionToBodyInPosts < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :posts, :caption, :body
-  end
+  # def change
+  #   # rename_column :posts, :caption, :body
+  # end
 end
