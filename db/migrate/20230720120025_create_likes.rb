2c84376b9c809e29c2c54fd78bc4cf1610c00beb
Koshin Hanaki <kousin.0512@gmail.com>
2023-08-03 20:00:11 +0900

    likeマイグレーションファイルを変更

--- db/migrate/20230720120025_create_likes.rb
+++ db/migrate/20230720120025_create_likes.rb
@@ -1,6 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    change_table :likes do |t|
+    create_table :likes do |t|
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
 
