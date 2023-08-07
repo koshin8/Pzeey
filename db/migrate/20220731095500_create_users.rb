88c21822563c577ca8393a222f6600dd173a4b69
Koshin Hanaki <kousin.0512@gmail.com>
2023-08-07 21:56:47 +0900

    config.hostにドメイン追加

--- db/migrate/20220731095500_create_users.rb
+++ db/migrate/20220731095500_create_users.rb
@@ -1,18 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
     def change
-      create_table :users do |t|
-        t.string :username, null: false
-        t.string :email, null: false
-        t.string :encrypted_password, null: false
-        t.string :reset_password_token
-        t.datetime :reset_password_sent_at
-        t.datetime :remember_created_at
-        t.timestamps
-      end
+      # create_table :users do |t|
+      #   t.string :username, null: false
+      #   t.string :email, null: false
+      #   t.string :encrypted_password, null: false
+      #   t.string :reset_password_token
+      #   t.datetime :reset_password_sent_at
+      #   t.datetime :remember_created_at
+      #   t.timestamps
+      # end
       
-      add_index :users, :username, unique: true
-      add_index :users, :email, unique: true
-      add_index :users, :reset_password_token, unique: true
+      # add_index :users, :username, unique: true
+      # add_index :users, :email, unique: true
+      # add_index :users, :reset_password_token, unique: true
     end
   end
   
--- db/migrate/20230720120019_create_posts.rb
+++ db/migrate/20230720120019_create_posts.rb
@@ -1,12 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
-    create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :body, null: false
-      t.float :latitude
-      t.float :longitude
-      t.timestamps
-    end
+    # create_table :posts do |t|
+    #   t.references :user, null: false, foreign_key: true
+    #   t.string :body, null: false
+    #   t.float :latitude
+    #   t.float :longitude
+    #   t.timestamps
+    # end
   end
 end
 
