3b04b1916ea8c67bdfc0d9e4ae2de250e72f76b9
Koshin Hanaki <kousin.0512@gmail.com>
2023-08-26 22:17:30 +0900

    モーダルウィンドウ実装

--- app/javascript/enlarge_image.js
+++ app/javascript/enlarge_image.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".post-image").forEach(function (image) {
-    image.addEventListener("click", function () {
-      this.classList.toggle("enlarged");
-    });
-  });
-});
+
